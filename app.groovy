1cdd35718a9e4cbaaaab91fd1505d0b9cad8af39
Curt Cox <curt.cox@asynchrony.com>
2017-04-18 11:04:52 -0500

    Delete unused json, application, services.

--- app.groovy
+++ app.groovy
@@ -6,17 +6,6 @@ import com.fasterxml.jackson.databind.*
 @Controller
 class Application {
 
-
-	@Autowired
-	private ObjectMapper json;
-
-	@Value('${VCAP_APPLICATION:{}}')
-	private String application;
-
-	@Value('${VCAP_SERVICES:{}}')
-	private String services;
-
-
 	@RequestMapping("/")
 	public String index(Model model) {
 		return "index"
